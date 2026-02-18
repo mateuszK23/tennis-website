c4361c1fbadffbcb8b416dabc3f851f6c4a34fda
Mateusz Koscielniak <matekk323@gmail.com>
2026-02-18 02:54:00 +0000

    Fixing stats

--- src/components/StatsTable.js
+++ src/components/StatsTable.js
@@ -17,7 +17,7 @@ export function renderStatsDiv(matches, container) {
       <thead>
         <tr>
           <th>Player</th>
-          <th>Won/Lost</th>
+          <th>Won</th>
           <th>Win Ratio</th>
         </tr>
       </thead>
@@ -27,7 +27,7 @@ export function renderStatsDiv(matches, container) {
   for (const player in stats) {
     html += `<tr>
       <td>${createFlag('pl')} <strong>${player}</strong></td>
-      <td>${stats[player].won}/${stats[player].played}</td>
+      <td>${stats[player].won}</td>
       <td>${stats[player].ratio}%</td>
     </tr>`;
   }
